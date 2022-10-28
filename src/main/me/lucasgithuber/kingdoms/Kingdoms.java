3537808c0cc6a94b7e2f69d67763dc7095cf201f
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-10-28 16:46:57 -0300

    trail

--- src/main/me/lucasgithuber/kingdoms/Kingdoms.java
+++ src/main/me/lucasgithuber/kingdoms/Kingdoms.java
@@ -1,15 +1,20 @@
 package me.lucasgithuber.kingdoms;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import me.lucasgithuber.kingdoms.listeners.*;
 import java.io.*;
 
 public class Kingdoms extends JavaPlugin {
+
+    private final CrownTrailManager crownTrailManager = new CrownTrailManager()
+
     @Override
     public void onEnable(){
-        System.out.println("Enabling plugin...");
+        JavaPlugin.getLogger("Enabling...");
+        Bukkit.getScheduler().runTaskTimer(this, crownTrailManager, 1, 1);
     }
 
     public void onDisable(){
-        System.out.println("Disabling plugin...");
+        JavaPlugin.getLogger("Disabling...");
     }
 }
---
+++ src/main/me/lucasgithuber/kingdoms/listeners/CrownTrailTask.java
@@ -0,0 +1,41 @@
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
+
+public class CrownTrailTask extends BukkitRunnable {
+
+    private final UUID playerID;
+    private final int waves;
+    private final double height;
+    private final double radius;
+    private int degree = 0;
+
+    public CrownTrailTask(UUID playerID, int waves, double height, double radius) {
+        this.playerID = playerID;
+        this.waves = waves;
+        this.height = height;
+        this.radius = radius;
+    }
+
+    @Override
+    public void run() {
+        degree %= 360;
+        Player player = Bukkit.getPlayer(playerID);
+        if (player == null) {
+            this.cancel();
+            return;
+        }
+        double radians = Math.toRadians(degree);
+        double x = Math.cos(radians) * radius;
+        double y = Math.sin(radians * waves) * height;
+        double z = Math.sin(radians) * radius;
+        Location particleLoc = player.getLocation().add(x, y, z);
+        player.getWorld().spawnParticle(Particle.END_ROD, particleLoc, 0);
+        degree += 5;
+    }
+
+}
