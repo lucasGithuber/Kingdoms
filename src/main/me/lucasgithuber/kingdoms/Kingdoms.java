07ceeb07c00d67f0e3d49534a4dc3efb5f5214c7
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-10-28 17:33:33 -0300

    log

--- src/main/me/lucasgithuber/kingdoms/Kingdoms.java
+++ src/main/me/lucasgithuber/kingdoms/Kingdoms.java
@@ -1,9 +1,11 @@
 package me.lucasgithuber.kingdoms;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 import me.lucasgithuber.kingdoms.listeners.CrownTrailTask;
 import me.lucasgithuber.kingdoms.listeners.CrownTrailManager;
 import java.io.*;
+import java.util.*;
 
 public class Kingdoms extends JavaPlugin {
 
@@ -11,11 +13,12 @@ public class Kingdoms extends JavaPlugin {
 
     @Override
     public void onEnable(){
-        JavaPlugin.getLogger().log(Level.INFO, "Enabling...");
+        Bukkit.getLogger().info("Enabling...");
         Bukkit.getScheduler().runTaskTimer(this, crownTrailManager, 1, 1);
     }
-
+    
+    @Override
     public void onDisable(){
-        JavaPlugin.getLogger().log(Level.INFO, "Disabling...");
+        Bukkit.getLogger().info("Disabling...");
     }
 }
