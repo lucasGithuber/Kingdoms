dee74b417bf1688ea4b9d3a1ff9b0f8a2cc8657c
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-10-31 17:21:22 -0300

    delay

--- src/main/me/lucasgithuber/kingdoms/Kingdoms.java
+++ src/main/me/lucasgithuber/kingdoms/Kingdoms.java
@@ -2,6 +2,7 @@ package me.lucasgithuber.kingdoms;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import me.lucasgithuber.kingdoms.listeners.CrownTrailTask;
 import me.lucasgithuber.kingdoms.listeners.CrownTrailManager;
 import me.lucasgithuber.kingdoms.commands.GiveCrown;
@@ -11,6 +12,7 @@ import java.util.*;
 
 public class Kingdoms extends JavaPlugin {
 
+    FileConfiguration config = getConfig();
     private final CrownTrailManager crownTrailManager = new CrownTrailManager();
 
     @Override
@@ -19,10 +21,18 @@ public class Kingdoms extends JavaPlugin {
         Bukkit.getLogger().info("Enabling...");
         Bukkit.getScheduler().runTaskTimer(this, crownTrailManager, 1, 1);
         getCommand("givecrown").setExecutor(new GiveCrown());
+
+        config.options().copyDefaults(true);
+        saveConfig();
+        Bukkit.getLogger().info(config.getString("test")+"is enabling");
     }
     
     @Override
     public void onDisable(){
         Bukkit.getLogger().info("Disabling...");
+    }
+
+    public FileConfiguration getConfigFile(){
+        return config;
     }
 }
