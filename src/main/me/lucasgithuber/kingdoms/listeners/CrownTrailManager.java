3dfa8f93e9a9c06618a4b0ed8c0e6ba78eac922c
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-10-31 19:11:53 -0300

    )

--- src/main/me/lucasgithuber/kingdoms/listeners/CrownTrailManager.java
+++ src/main/me/lucasgithuber/kingdoms/listeners/CrownTrailManager.java
@@ -22,7 +22,7 @@ public class CrownTrailManager implements Runnable {
     @Override
     public void run() {
         for(Player player : Bukkit.getOnlinePlayers()){
-        runningTasks.put(player, new CrownTrailTask(8, 2, 2);
+        runningTasks.put(player, new CrownTrailTask(8, 2, 2));
         runningTasks.values().forEach(CrownTrailTask::run);
         }
     }
