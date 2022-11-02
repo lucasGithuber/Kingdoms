2254a7cc23a88693fb114d5b688553d5c4b3f868
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-11-02 16:10:21 -0300

    crown

--- src/main/me/lucasgithuber/kingdoms/listeners/TrailsManager.java
+++ src/main/me/lucasgithuber/kingdoms/listeners/TrailsManager.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.Bukkit;
 import me.lucasgithuber.kingdoms.listeners.TrailsTask;
+import me.lucasgithuber.kingdoms.items.Crown;
 
 import java.util.HashMap;
 import java.util.Map;
