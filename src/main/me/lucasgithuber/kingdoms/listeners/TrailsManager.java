f6856abe8533e69544df53c7d59785987c1fd508
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-11-02 16:13:47 -0300

    issimilar

--- src/main/me/lucasgithuber/kingdoms/listeners/TrailsManager.java
+++ src/main/me/lucasgithuber/kingdoms/listeners/TrailsManager.java
@@ -24,7 +24,7 @@ public class TrailsManager implements Runnable {
     @Override
     public void run() {
         for(Player player : Bukkit.getOnlinePlayers()){
-            if(player.getInventory().getHelmet() instanceof Crown){
+            if(player.getInventory().getHelmet().isSimilar(Crown)){
                 runningTasks.put(player, new TrailsTask(8, 0.5, 1));
                 }
             runningTasks.values().forEach(TrailsTask::run);
