ec0c2af63dbec33fa8673b80d3ca9f90c5e8895c
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-11-05 16:48:24 -0300

    hehe

--- src/main/me/lucasgithuber/kingdoms/testgui/TestGuiListener.java
+++ src/main/me/lucasgithuber/kingdoms/testgui/TestGuiListener.java
@@ -1,34 +1,28 @@
 package me.lucasgithuber.kingdoms.testgui;
 
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.inventory.ItemStack;
 
 public class TestGuiListener implements Listener {
 
     @EventHandler
-    public void clickEvent(InventoryClickEvent e){
-
-        //check if its the GUI menu
-        if(e.getView().getTitle().equalsIgnoreCase(ChatColor.AQUA + "Custom GUI")){
-            Player player = (Player) e.getWhoClicked();
-            //check what they click
-            switch(e.getCurrentItem().getType()){
-                case BARRIER:
-                    player.closeInventory();
-                    break;
-                case DIAMOND_SWORD:
-                    player.closeInventory();
-                    player.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD));
-                    break;
+    public void onClick(InventoryClickEvent e){
+        Player player = (Player) e.getWhoClicked();
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE "Particles")){
+            e.setCancelled(true);
+            if (e.getCurrentItem() == null){
+                return;
+            }else if (e.getCurrentItem().getType().equals(Material.CHIPPED_ANVIL)){
+                player.closeInventory();
+            }else if (e.getCurrentItem().getType().equals(Material.CYAN_CARPET)){
+                player.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD));
+                player.closeInventory();
             }
-            e.setCancelled(true); //dont take item
         }
-
     }
-
 }
