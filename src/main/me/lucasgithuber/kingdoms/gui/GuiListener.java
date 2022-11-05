69e306bfb029397232f7667d9ca513004bc15037
lucasGithuber <91435998+lucasGithuber@users.noreply.github.com>
2022-11-05 14:37:15 -0300

    dbug

--- src/main/me/lucasgithuber/kingdoms/gui/GuiListener.java
+++ src/main/me/lucasgithuber/kingdoms/gui/GuiListener.java
@@ -2,8 +2,11 @@ package me.lucasgithuber.kingdoms.gui;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.entity.Player;
+import java.io.*;
+import java.util.*;
 
 import me.lucasgithuber.kingdoms.gui.*;
 
@@ -19,6 +22,8 @@ public class GuiListener implements Listener{
 
                 e.setCancelled(true);
             }
+        }else{
+            Bukkit.getLogger().info("data or openGui or currentItem is null");
         }
     }
 }
